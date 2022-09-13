99fb7428c1b58ed7b2e7b55f4df5df90c6e1c2ab
Robertstein <robertovenezia95@gmail.com>
2022-09-13 13:09:30 +0200

    Implemented Steam Transport Compatibility

--- NetworkingLesson/Assets/Scripts/RTS/Menus/MainMenu.cs
+++ NetworkingLesson/Assets/Scripts/RTS/Menus/MainMenu.cs
@@ -1,16 +1,78 @@
 using System.Collections;
 using System.Collections.Generic;
 using Mirror;
+using Steamworks;
 using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] private GameObject landingPagePanel = null;
 
+    [SerializeField] private bool useSteam = false;
+
+    protected Callback<LobbyCreated_t> lobbyCreated;
+    protected Callback<GameLobbyJoinRequested_t> gameLobbyJoinRequested;
+    protected Callback<LobbyEnter_t> lobbyEntered;
+
+    private void Start()
+    {
+        if (!useSteam) { return; }
+
+        lobbyCreated = Callback<LobbyCreated_t>.Create(OnLobbyCreated);
+        gameLobbyJoinRequested = Callback<GameLobbyJoinRequested_t>.Create(OnGameLobbyJoinRequested);
+        lobbyEntered = Callback<LobbyEnter_t>.Create(OnLobbyEntered);
+    }
+
     public void HostLobby()
     {
         landingPagePanel.SetActive(false);
 
+        if (useSteam)
+        {
+            //Creo la lobby. Qua ho hardcodato che la lobby sia solo per gli amici, con un massimo di 4 player. Ovviamente si può customizzare
+            SteamMatchmaking.CreateLobby(ELobbyType.k_ELobbyTypeFriendsOnly, 4);
+            //Ovviamente se uso la lobby di steam volgio aspettare che questa sia creata per poter hostare, quindi per ora faccio return
+            //Avvierò l'hosting dalla Callback
+            return;
+        }
+
         NetworkManager.singleton.StartHost();
+    }
+
+    private void OnLobbyCreated(LobbyCreated_t callback)
+    {
+        //Se steam ha fallito nel creare la lobby torno indietro
+        if (callback.m_eResult != EResult.k_EResultOK)
+        {
+            landingPagePanel.SetActive(true);
+            return;
+        }
+
+        //In caso contrario inizio a hostare
+        NetworkManager.singleton.StartHost();
+
+        //Ci serve settare l'ID della lobby per permettere ai nostri amici di joinare. Per crearlo usiamo una apposita funzione di Steamworks,
+        //passandogli il nostro User SteamID
+        SteamMatchmaking.SetLobbyData(new CSteamID(callback.m_ulSteamIDLobby), "HostAddress", SteamUser.GetSteamID().ToString());
+    }
+
+    private void OnGameLobbyJoinRequested(GameLobbyJoinRequested_t callback)
+    {
+        //Quando un amico cerca di joinare lo facciamo semplicemente joinare
+        SteamMatchmaking.JoinLobby(callback.m_steamIDLobby);
+    }
+
+    private void OnLobbyEntered(LobbyEnter_t callback)
+    {
+        //Se chi è entrato nella lobby è l'host faccio return
+        if (NetworkServer.active) { return; }
+
+        //Chiunque entri nella lobby grabba il mio steamID
+        string hostAddress = SteamMatchmaking.GetLobbyData(new CSteamID(callback.m_ulSteamIDLobby), "HostAddress");
+        //E lo passa al NetworkManager, per poi startare il client
+        NetworkManager.singleton.networkAddress = hostAddress;
+        NetworkManager.singleton.StartClient();
+
+        landingPagePanel.SetActive(false);
     }
 }
--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # Multiplayer-Online-RTS
 Prototype of a multiplayer online RTS made as exercise
 
+CONTROLS
 To spawn buildings simply drag them from the UI into the level. They must stay close to another allied building to be placed succesfully.
 To spawn units from the buildings, click on the building of choice.
+
+
+Steamworks releases: https://github.com/rlabrecque/Steamworks.NET/releases
+FizzySteamworks releases:  https://github.com/Chykary/FizzySteamworks/releases
+
+To Make the build work via Steam:
+1. check "Use Steam" in the MainMenuDisplay in the editor
+2. copy the the file  “steam_appid.txt” into the build folder
+3. open Steam, go to Games->Add a Non-Steam Game to My Library and select the .exe file from the build folder
